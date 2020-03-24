87435042ef91adef16c3fa6d728d9fdb33d3d371
Andrew Galyan-Mann <andrew.galyanmann@icloud.com>
2020-03-24 17:21:04 -0400

    Import data to the projects route

--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { projects } = require("../data/data.json");
 
-router.get("/", (req, res) => {
-  res.render("project");
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const { project_name } = projects[id];
+  const { description } = projects[id];
+  const { technologies } = projects[id];
+  const templateData = { project_name, description, technologies };
+  res.render("project", templateData);
 });
 
 module.exports = router;
