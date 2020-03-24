1d004e65aa665e41d3d13481895f724a7ed7c3cb
Andrew Galyan-Mann <andrew.galyanmann@icloud.com>
2020-03-24 15:12:47 -0400

    Organize initial file tree, start Express, create root rout

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use('/static', express.static('public'));
-app.set('view engin', 'pug');
+app.use("/static", express.static("public"));
+app.set("view engine", "pug");
+
+const routes = require("./routes");
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
