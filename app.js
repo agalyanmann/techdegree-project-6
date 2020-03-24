39b7b5a46e83b28ac8a21fce94a0392e4c644a4d
Andrew Galyan-Mann <andrew.galyanmann@icloud.com>
2020-03-24 15:33:10 -0400

    Add routes initial routes for index.js, about.js, projects.js

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const app = express();
 app.use("/static", express.static("public"));
 app.set("view engine", "pug");
 
-const routes = require("./routes");
-app.use(routes);
+const mainRoutes = require("./routes");
+const aboutRoutes = require("./routes/about");
+const projectRoutes = require("./routes/projects");
+
+app.use(mainRoutes);
+app.use("/about", aboutRoutes);
+app.use("/projects", projectRoutes);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
