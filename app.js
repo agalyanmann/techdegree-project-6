708eeb6f34df5b0ac391d564c2dccf54ade23b8b
Andrew Galyan-Mann <andrew.galyanmann@icloud.com>
2020-05-19 23:09:32 -0400

    link error routes and middleware

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ app.use(mainRoutes);
 app.use("/about", aboutRoutes);
 app.use("/projects", projectRoutes);
 
+app.use((req, res, next) => {
+  const err = new Error("Page Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  console.log(err);
+  res.render("error");
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
