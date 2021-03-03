a5947c349408cb4749861d91b6345503596ce4bd
Andrew Galyan-Mann <asgalyanmann@gmail.com>
2021-03-03 00:06:32 -0500

    update port variable in app.js

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.use((err, req, res, next) => {
   res.render("error");
 });
 
-// app.listen(3000, () => {
-//   console.log("Listening on port 3000");
-// });
+const port = process.env.PORT;
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
