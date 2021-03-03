8de9b6936c36d473be22c0ae987c2a0f7127eb71
Andrew Galyan-Mann <asgalyanmann@gmail.com>
2021-03-03 00:00:03 -0500

    comment out port

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ app.use((err, req, res, next) => {
   res.render("error");
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Listening on port 3000");
+// });
